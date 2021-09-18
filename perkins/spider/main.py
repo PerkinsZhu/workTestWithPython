4c4a3daa50e4c3e60ae5647ed60bc19a0a8f9a70
zhupingjing <zhupingjing@bersonfintech.com>
2021-09-18 17:49:43 +0800

    some test

--- perkins/spider/main.py
+++
@@ -1,4 +0,0 @@
-import scrapy
-
-if __name__ == '__main__':
-    url = ['https://blog.scrapinghub.com']
--- perkins/tianChiMatch/testOne/testAgain.py
+++ perkins/tianChiMatch/testOne/testAgain.py
@@ -1,4 +1,17 @@
 '''
  Created by PerkinsZhu on 2017/11/11 17:06. 
 '''
+import pandas as pd
 
+"""
+ behavior_type  用户对商品的行为类型  包括浏览、收藏、加购物车、购买，对应取值分别是1、2、3、4。
+"""
+
+
+def loadData():
+    df = pd.read_csv(r'D:\test\tianchi\data01\fresh_comp_offline\user.csv')
+    # 买过相同的物品，越多则越相似
+
+
+def test():
+    loadData()
