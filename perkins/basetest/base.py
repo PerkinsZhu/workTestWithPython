c7659ad112d981a6fc265877b99ea214aeafd9c1
zhupingjing <zhupingjing@bersonfintech.com>
2021-09-18 15:42:36 +0800

    some test

--- perkins/basetest/base.py
+++ perkins/basetest/base.py
@@ -111,3 +111,12 @@ class InnerClass:
     def __init__(self, name):
         print("触发init方法", name)
         self.name = name
+
+
+def test_game_01():
+    print('\n'.join([''.join([('WangGangdan'[(x - y) % 10] if ((x * 0.05) ** 2 + (y * 0.1) ** 2 - 1) ** 3 - (
+            x * 0.05) ** 2 * (y * 0.1) ** 3 <= 0 else ' ') for x in range(-30, 30)]) for y in range(15, -15, -1)]))
+
+
+def test_game_02():
+    print('\n'.join([''.join(['*'if abs((lambda a:lambda z,c,n:a(a,z,c,n))(lambda s,z,c,n:z if n==0else s(s,z*z+c,c,n-1))(0,0.02*x+0.05j*y,40))<2 else' 'for x in range(-80,20)])for y in range(-20,20)]))
